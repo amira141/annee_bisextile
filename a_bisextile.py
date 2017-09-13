00598310dd7a6172c1ef1b74bf9e8cd8bedf6505
Amira <amira141592@gmail.com>
2017-09-13 13:33:27 +0300

    boucle qui permet de déterminer si une année est bisextile ou non

--- a_bisextile.py
+++ a_bisextile.py
@@ -2,10 +2,15 @@
 
 #Déterminer si une année saisie par l'utilisateur est bissextile.
 
-u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ")
+u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ") #L'utilisateur rentre une année
 A = int(u_a)
 
-if A%4 != 0:
-	print("L'année n'est pas bisextile.") 
-else: 
-	print("L'année est bisextille.")
+if A%4 == 0 and A%100 != 0 :  
+    print("L'année est bisextile.")
+ 
+elif A%400 == 0: 
+    print("L'année est bisextile")
+
+else:
+    print("L'année nest pas bisextile") 
+
