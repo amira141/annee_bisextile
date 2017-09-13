35110fa9512e199e276a8df3d65f62ce5642991d
Amira <amira141592@gmail.com>
2017-09-13 18:38:37 +0300

    amelioration detection erreur

--- a_bisextile.py
+++ a_bisextile.py
@@ -19,8 +19,12 @@ import os #Module qui va importer une fonction pour permettre d'afficher le prog
 u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ") #L'utilisateur rentre une année
 try:
 	A = int(u_a)
-except:
-	print("Veuillez resaisir une année correcte SVP.")
+	assert A>0
+except ValueError:
+	print("Vous n'avez pas saisi un nombre.")
+except AssertionError:
+	print("L'année saisie est inférieure ou égale à 0.")
+#Reste a definir comment ne pas afficher la suite du code si l'année est inférieur ou égale à 0.
 
 if A%400 == 0 or (A%100 != 0 and A%4 == 0) : #Si l'année est un multiple de 4 et pas de 100 ou bien qu'elle est un multiple de 400, elle est bisextile
     print("L'année est bisextile.")
