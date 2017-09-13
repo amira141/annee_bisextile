7fdc52ecc9a4f67bac9563c4b26da74876ad659c
Amira <amira141592@gmail.com>
2017-09-13 13:36:19 +0300

    ajout de commentaires explicatifs

--- a_bisextile.py
+++ a_bisextile.py
@@ -1,16 +1,25 @@
 #***ANNEE BISEXTILE***
 
+#***************************************
+
+#Une année est bisextile uniquement si :
+#elle est un multiple de  4 et pas de 100
+#OU 
+#elle est un multiple de 400 
+
+#***************************************
+
 #Déterminer si une année saisie par l'utilisateur est bissextile.
 
 u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ") #L'utilisateur rentre une année
 A = int(u_a)
 
-if A%4 == 0 and A%100 != 0 :  
+if A%4 == 0 and A%100 != 0 :  #Si l'année est un multiple de 4 et pas de 100, elle est bisextile
     print("L'année est bisextile.")
  
-elif A%400 == 0: 
+elif A%400 == 0: #Sinon si elle est un multiple de 400 elle est bisextile 
     print("L'année est bisextile")
 
-else:
+else: #Sinon elle n'est pas bisextile 
     print("L'année nest pas bisextile") 
 
