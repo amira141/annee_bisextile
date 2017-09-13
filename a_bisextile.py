e79300159620d7986fb5a8610dcd66fd36bcdc84
Amira <amira141592@gmail.com>
2017-09-13 13:39:47 +0300

    simplification de la boucle grace au mot cle or

--- a_bisextile.py
+++ a_bisextile.py
@@ -14,11 +14,8 @@
 u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ") #L'utilisateur rentre une année
 A = int(u_a)
 
-if A%4 == 0 and A%100 != 0 :  #Si l'année est un multiple de 4 et pas de 100, elle est bisextile
+if A%4 == 0 and A%100 != 0 or A%400 == 0 : #Si l'année est un multiple de 4 et pas de 100 ou bien qu'elle est un multiple de 400, elle est bisextile
     print("L'année est bisextile.")
- 
-elif A%400 == 0: #Sinon si elle est un multiple de 400 elle est bisextile 
-    print("L'année est bisextile")
 
 else: #Sinon elle n'est pas bisextile 
     print("L'année nest pas bisextile") 
