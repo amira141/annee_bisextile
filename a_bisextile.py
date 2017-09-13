796e95f288b842960825eea131bc81fb8c2b6932
Amira <amira141592@gmail.com>
2017-09-13 16:17:19 +0300

    modification du predicat avec ajout de parentheses

--- a_bisextile.py
+++ a_bisextile.py
@@ -14,7 +14,7 @@
 u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ") #L'utilisateur rentre une année
 A = int(u_a)
 
-if A%4 == 0 and A%100 != 0 or A%400 == 0 : #Si l'année est un multiple de 4 et pas de 100 ou bien qu'elle est un multiple de 400, elle est bisextile
+if A%400 == 0 or (A%100 != 0 and A%4 == 0) : #Si l'année est un multiple de 4 et pas de 100 ou bien qu'elle est un multiple de 400, elle est bisextile
     print("L'année est bisextile.")
 
 else: #Sinon elle n'est pas bisextile 
