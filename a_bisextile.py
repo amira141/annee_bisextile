a0c689a46faaa073d7143dc7c827ece3c03166d2
Amira <amira141592@gmail.com>
2017-09-13 12:45:01 +0300

    suppression des guillements en trop

--- a_bisextile.py
+++ a_bisextile.py
@@ -2,7 +2,7 @@
 
 #Déterminer si une année saisie par l'utilisateur est bissextile.
 
-u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ")
+u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non")
 A = int(u_a)
 
 if A%4 != 0:
