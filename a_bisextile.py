a33f58fa8bedf77f743d16834b8fdf8197ba4388
Amira <amira141592@gmail.com>
2017-09-13 12:44:23 +0300

    definir le type de variable int pour le bon fonctionnement du predicat

--- a_bisextile.py
+++ a_bisextile.py
@@ -2,7 +2,10 @@
 
 #Déterminer si une année saisie par l'utilisateur est bissextile.
 
-A = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ")
+u_a = input("Ecrivez une année pour savoir si celle-ci est bisextile ou non"" ")
+A = int(u_a)
 
-if : 
+if A%4 != 0:
+	print("L'année n'est pas bisextile.") 
 else: 
+	print("L'année est bisextille.")
